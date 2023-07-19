91173c84a44a96b21263ddf902bcd30e28b001de
nedkamburov <nedislav.kamburov@gmail.com>
2023-07-19 22:37:14 +0300

    add mobile functionality for the navigation

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,37 +1,45 @@
 import Link from "next/link";
 import { SimplePage } from "../types/contentTypes";
 import ThemeSwitcher from "./UI/ThemeSwitcher";
+import Iconly, { icons } from "./UI/Iconly";
 
 export default function Navbar({
   websiteDetails,
 }: {
   websiteDetails: { title: string; description: string; navigationPages: { data: SimplePage[] } };
 }) {
-  console.log(websiteDetails);
   const { title, navigationPages } = websiteDetails;
   return (
     <nav>
       <div className="website-name">
         <Link href={`/`}>{title}</Link>
       </div>
-      <ul className="pages">
-        <li>
-          <Link href={`/`}>Typefaces</Link>
-        </li>
-        {navigationPages.data.map((page: SimplePage) => (
-          <li key={page.id}>
-            <Link href={`/${page.attributes.slug}`}>{page.attributes.title}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="settings">
-        <div className="theme-switcher">
-          <ThemeSwitcher />
-        </div>
-        <div className="cart">
-          <Link href={`/cart`}>Cart</Link>
+      <div className="mobile-nav">
+        <span className="close">
+          Close <Iconly icon={icons.close} />
+        </span>
+        <div className="inner-nav">
+          <ul className="pages">
+            <li>
+              <Link href={`/`}>Typefaces</Link>
+            </li>
+            {navigationPages.data.map((page: SimplePage) => (
+              <li key={page.id}>
+                <Link href={`/${page.attributes.slug}`}>{page.attributes.title}</Link>
+              </li>
+            ))}
+          </ul>
+          <div className="settings">
+            <div className="theme-switcher">
+              <ThemeSwitcher />
+            </div>
+            <div className="cart">
+              <Link href={`/cart`}>Cart</Link>
+            </div>
+          </div>
         </div>
       </div>
+      <Iconly icon={icons.hamburger} />
     </nav>
   );
 }
