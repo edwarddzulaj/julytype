3040776f6cd7982db0917b0a3215b893157d5091
nedkamburov <nedislav.kamburov@gmail.com>
2023-07-19 20:31:12 +0300

    add wording to styles and weights

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,17 +19,26 @@ export default async function Page() {
   const typefaces = await getTypefaces();
   return (
     <section className="typeface-preview">
-      {typefaces.map((typeface: Typeface) => (
-        <Link href={`/typefaces/${typeface.attributes.slug}`} key={typeface.id}>
-          <article>
-            {typeface.attributes.title}
-            <div className="typeface-details">
-              <span>{typeface.attributes.styles.data.length}</span>
-              <span>{typeface.attributes.styles.data[0].attributes.weights.length}</span>
-            </div>
-          </article>
-        </Link>
-      ))}
+      {typefaces.map((typeface: Typeface) => {
+        const numStyles = typeface.attributes.styles.data.length;
+        const numWeights = typeface.attributes.styles.data[0].attributes.weights.length;
+        return (
+          <Link href={`/typefaces/${typeface.attributes.slug}`} key={typeface.id}>
+            <article>
+              {typeface.attributes.title}
+              <div className="typeface-details">
+                <span>
+                  {numStyles} {numStyles > 1 ? "styles" : "style"}
+                </span>
+                <br />
+                <span>
+                  {numWeights} {numWeights > 1 ? "weights" : "weight"}
+                </span>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
     </section>
   );
 }
