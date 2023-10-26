454310f8895a52d65e512482ef8e58a997f89886
nedkamburov <nedislav.kamburov@gmail.com>
2023-10-26 20:21:28 +0300

    add a mobile menu

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import { SimplePage } from "../../@types/contentTypes";
 import ThemeSwitcher from "./UI/ThemeSwitcher";
 import Iconly, { icons } from "./UI/Iconly";
+import { useState } from "react";
 
 export default function Navbar({
   websiteDetails,
@@ -9,12 +12,17 @@ export default function Navbar({
   websiteDetails: { title: string; description: string; navPages: { data: SimplePage[] } };
 }) {
   const { title, navPages } = websiteDetails;
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
   return (
     <nav>
       <div className="website-name">
         <Link href={`/`}>{title}</Link>
       </div>
-      <div className="inner-nav">
+      <div className={`inner-nav ${isMenuActive ? "active" : ""}`}>
         <ul className="pages">
           <li>
             <Link href={`/`}>Typefaces</Link>
@@ -26,20 +34,24 @@ export default function Navbar({
           ))}
         </ul>
         <div className="settings">
-          <div className="theme-switcher">
-            <ThemeSwitcher />
+          <div className="inner-settings">
+            <div className="theme-switcher">
+              <ThemeSwitcher />
+            </div>
+            <div className="cart">
+              <Link href={`/cart`}>Cart</Link>
+            </div>
           </div>
-          <div className="cart">
-            <Link href={`/cart`}>Cart</Link>
-          </div>
+          <button className="close" onClick={toggleMenu}>
+            Close <Iconly icon={icons.close} />
+          </button>
         </div>
       </div>
-      {/* <div className="mobile-nav">
-        <span className="close">
-          Close <Iconly icon={icons.close} />
-        </span>
-        <Iconly icon={icons.hamburger} />
-      </div> */}
+      <div className="mobile-nav">
+        <button onClick={toggleMenu}>
+          <Iconly icon={icons.hamburger} />
+        </button>
+      </div>
     </nav>
   );
 }
