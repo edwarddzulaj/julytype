f8b0a68ebcd7470bb31248ceb50b43af6c7445f2
nedkamburov <nedislav.kamburov@gmail.com>
2023-11-25 19:04:36 +0200

    move the stripe session creation to an order controller

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -1,9 +1,77 @@
-'use strict';
+("use strict");
+const STRIPE_KEY =
+  process.env.STRAPI_ENV === "production"
+    ? process.env.STRIPE_SECRET_KEY
+    : process.env.STRIPE_TEST_SECRET_KEY;
 
+// @ts-ignore
+const stripe = require("stripe")(STRIPE_KEY);
+
+const CLIENT_URL =
+  process.env.STRAPI_ENV === "production"
+    ? process.env.CLIENT_URL
+    : process.env.CLIENT_TEST_URL;
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products } = ctx.request.body;
+
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const typeface = await strapi
+            .service("api::typeface.typeface")
+            .findOne(product.id);
+
+          const weightsNames = product.weights
+            .map((weight) => weight.title)
+            .join(", ");
+          return {
+            price_data: {
+              currency: "eur",
+              product_data: {
+                name: `JT ${product.name}`,
+                description: `Your selected weights from JT ${product.name}: ${weightsNames}`,
+                metadata: {
+                  weights: weightsNames,
+                },
+              },
+              unit_amount: product.totalPrice * 100,
+            },
+            quantity: 1,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: "payment",
+        custom_text: {
+          submit: {
+            message:
+              "We'll email you the font package after the payment is completed.",
+          },
+        },
+        success_url: `${CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: CLIENT_URL,
+      });
+
+      await strapi.service("api::order.order").create({
+        data: {
+          stripeId: session.id,
+          products: lineItems,
+        },
+      });
+
+      return { stripeSession: session };
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error };
+    }
+  },
+}));
--- frontend/app/cart/api/checkout_sessions/route.ts
+++ frontend/app/cart/api/checkout_sessions/route.ts
@@ -1,45 +1,17 @@
 import { NextResponse, NextRequest } from "next/server";
-import { headers } from "next/headers";
-import stripe from "@/app/config/stripe";
-import { ProductItem } from "@/app/redux/cartReducer";
+import { fetchAPI } from "@/app/utils/fetch-api";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const headersList = headers();
   const { cartItems } = await req.json();
 
-  const lineItems = cartItems.map((item: ProductItem) => {
-    const weightsNames = item.weights.map((weight) => weight.title).join(", ");
-
-    return {
-      price_data: {
-        currency: "eur",
-        product_data: {
-          name: `JT ${item.name}`,
-          description: `Your selected weights from JT ${item.name}: ${weightsNames}`,
-          metadata: {
-            weights: weightsNames,
-          },
-        },
-        unit_amount: item.totalPrice! * 100,
-      },
-      quantity: 1,
-    };
-  });
-
   try {
-    const session = await stripe.checkout.sessions.create({
-      line_items: lineItems,
-      mode: "payment",
-      custom_text: {
-        submit: {
-          message: "We'll email you the font package after the payment is completed.",
-        },
-      },
-      success_url: `${headersList.get("origin")}/success`,
-      cancel_url: `${headersList.get("origin")}/`,
-    });
+    const response = await fetchAPI(
+      "/orders",
+      {},
+      { method: "POST", body: JSON.stringify({ products: cartItems }) }
+    );
 
-    return NextResponse.json({ sessionId: session.id });
+    return NextResponse.json({ sessionId: response.stripeSession.id });
   } catch (err) {
     console.error(err);
     return NextResponse.json({ error: "Error creating checkout session" });
