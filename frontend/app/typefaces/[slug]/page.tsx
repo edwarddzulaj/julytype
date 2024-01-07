ffacbff7826e2ff815a6ef13b9f79013787e3a46
nedkamburov <nedislav.kamburov@gmail.com>
2024-01-07 16:52:08 +0200

    use only typeface page template instead of style page

--- frontend/app/typefaces/[slug]/page.tsx
+++ frontend/app/typefaces/[slug]/page.tsx
@@ -1,17 +1,19 @@
-import Link from "next/link";
-import Markdown from "react-markdown";
+import { TypefaceWeight } from "@/@types/components";
+import { Typeface, Style } from "@/@types/contentTypes";
 import { fetchAPI } from "@/app/utils/fetch-api";
-import { Style, Typeface } from "@/@types/contentTypes";
 import { getStrapiMedia } from "@/app/utils/api-helpers";
-import BackButton from "@/app/components/UI/BackButton";
+
 import BuyButton from "@/app/components/UI/BuyButton";
+import ChooseScript from "@/app/components/Typeface/ChooseScript";
+import { FontsData } from "@/app/components/Typeface/Typetester/typetester-types";
+import Markdown from "react-markdown";
+import OpentypeFeaturesPreview from "@/app/components/Typeface/OpentypeFeaturesPreview/OpentypeFeaturesPreview";
+import PDFPreview from "@/app/components/Typeface/PDFPreview";
 import PurchaseSectionWrapper from "@/app/components/Cart/PurchaseSection/PurchaseSectionWrapper";
 import Section from "@/app/components/UI/Section";
+import { ScriptChoiceProvider } from "@/app/providers";
+import SupportedLanguages from "@/app/components/Typeface/SupportedLanguages";
 import Typetester from "@/app/components/Typeface/Typetester/Typetester";
-import PDFPreview from "@/app/components/Typeface/PDFPreview";
-import { FontsData } from "@/app/components/Typeface/Typetester/typetester-types";
-import { TypefaceWeight } from "@/@types/components";
-import TypefaceSample from "@/app/components/Typeface/TypefaceSample";
 
 async function getTypeface(slug: string) {
   const path = `/typefaces`;
@@ -27,6 +29,7 @@ async function getTypeface(slug: string) {
           },
         },
       },
+      supportedLanguages: { populate: "*" },
       specimen: { populate: "*" },
       trialFonts: { populate: "*" },
     },
@@ -36,81 +39,79 @@ async function getTypeface(slug: string) {
   };
 
   const responseData = await fetchAPI(path, urlParamsObject);
-
   return responseData.data[0];
 }
 
 export default async function Typeface({ params }: { params: { slug: string } }) {
-  const typeface: Typeface = await getTypeface(params.slug);
-  const { title, slug, specimen, aboutText, styles, trialFonts } = typeface.attributes;
-
+  const { slug } = params;
+  const typeface: Typeface = await getTypeface(slug);
+  const typefaceTitle = typeface.attributes.title;
+  const { specimen, aboutText, styles, trialFonts, supportedLanguages } = typeface.attributes;
   const hasTrialFonts = trialFonts?.data?.length > 0;
-  const typetesterFontsData = constructFontData(typeface);
-  const sampleStyle = styles.data[0].attributes;
+
+  const styleSlug = styles.data[0].attributes.slug;
+  const style: Style | undefined = typeface.attributes.styles.data.find(
+    (style) => style.attributes.slug === styleSlug
+  );
+  const { title, weights, lineHeight } = style?.attributes ?? { title: "", weights: [] };
+
+  const regularWeight = weights.find((w) => w.title.toLowerCase().includes("regular"));
+  const opentypeFeaturesFontTitle = `${title.trim()} ${regularWeight?.title.trim()}`;
 
   return (
-    <section className="container typeface">
-      <article className="quick-buttons">
-        <BackButton>Back to Typefaces</BackButton>
-        <div className="action-buttons">
-          {/* {hasTrialFonts && <TrialFontsButton/>} */}
-          <BuyButton>Buy {title}</BuyButton>
-        </div>
-      </article>
-      <Section title="Overview">
-        <article className="typeface-styles">
-          {styles.data.map((style: Style) => (
-            <Link href={`/typefaces/${slug}/${style?.attributes?.slug}`} key={style.id}>
-              <TypefaceSample
-                title={style?.attributes?.title}
-                regularWeight={style?.attributes?.weights[0]}
-              />
-            </Link>
-          ))}
+    <ScriptChoiceProvider>
+      <section className="container style">
+        <article className="quick-buttons">
+          <div className="action-buttons">
+            {/* {hasTrialFonts && <TrialFontsButton/>} */}
+            <BuyButton>{`Buy ${title}`}</BuyButton>
+          </div>
         </article>
-      </Section>
-      <Section title="Specimen">
-        <section className="typetesters">
-          <Typetester
-            fontsData={typetesterFontsData}
-            typetesterLanguageGroup={sampleStyle.weights[0].typetesterLanguageGroup}
-            defaultOptions={{ lineHeight: sampleStyle.lineHeight }}
-          />
-        </section>
-        {specimen?.data?.attributes?.url && (
-          <section className="download">
-            <PDFPreview url={getStrapiMedia(specimen.data.attributes.url)} />
+        <ChooseScript />
+        <Section title="Overview">
+          <section className="typetesters">
+            {weights.map((weight: TypefaceWeight) => {
+              const fontLabel = `${title.trim()} ${weight.title.trim()}`;
+              const typetesterData: FontsData = {
+                label: fontLabel,
+                value: btoa(weight.title),
+                fontPath: getStrapiMedia(weight.fontFile?.data?.attributes?.url),
+              };
+
+              return (
+                <Typetester
+                  key={weight.id}
+                  fontsData={[typetesterData]}
+                  typetesterLanguageGroup={weight.typetesterLanguageGroup}
+                  defaultOptions={{ lineHeight: lineHeight }}
+                />
+              );
+            })}
           </section>
+        </Section>
+        {specimen?.data?.attributes?.url && (
+          <Section title="Specimen">
+            <section className="download">
+              <PDFPreview url={getStrapiMedia(specimen.data.attributes.url)} />
+            </section>
+          </Section>
         )}
-      </Section>
-      <Section title="About">
-        {/* eslint-disable-next-line react/no-children-prop */}
-        <Markdown children={aboutText} />
-      </Section>
-      <Section title={`Buy ${title}`} noIndent={true}>
-        <PurchaseSectionWrapper typeface={typeface} />
-      </Section>
-    </section>
+        <Section title={`About ${typefaceTitle}`}>
+          {/* eslint-disable-next-line react/no-children-prop */}
+          <Markdown children={aboutText} />
+        </Section>
+        <Section title={`Opentype features preview`}>
+          <OpentypeFeaturesPreview fontFamilyTitle={opentypeFeaturesFontTitle} />
+        </Section>
+        {supportedLanguages && supportedLanguages.length > 0 && (
+          <Section title={`Supported Languages`}>
+            <SupportedLanguages languageData={supportedLanguages} />
+          </Section>
+        )}
+        <Section title={`Buy ${typefaceTitle}`} noIndent={true}>
+          <PurchaseSectionWrapper typeface={typeface} />
+        </Section>
+      </section>
+    </ScriptChoiceProvider>
   );
 }
-
-const constructFontData = (typeface: Typeface) => {
-  let typetesterFontsData: FontsData[] = [];
-
-  const { styles } = typeface.attributes;
-
-  styles.data.map((style: Style) => {
-    style.attributes.weights.map((weight: TypefaceWeight) => {
-      const fontLabel = `${style.attributes.title.trim()} ${weight.title.trim()}`;
-      const fontValue = btoa(fontLabel);
-
-      typetesterFontsData.push({
-        label: fontLabel,
-        value: fontValue,
-        fontPath: getStrapiMedia(weight.fontFile?.data?.attributes?.url),
-      });
-    });
-  });
-
-  return typetesterFontsData;
-};
