a341a58debdb059aa84906dc2e12adc8502c282d
nedkamburov <nedislav.kamburov@gmail.com>
2024-02-02 11:19:57 +0200

    adjust tooltip behaviour and styling

--- frontend/app/components/UI/Tooltip.tsx
+++ frontend/app/components/UI/Tooltip.tsx
@@ -3,10 +3,19 @@ import Iconly, { icons } from "./Iconly";
 
 export function Tooltip({ id, content }: { id: string; content: string }) {
   return (
-    <span data-tooltip-id={id} data-tooltip-content={content} data-tooltip-class-name="tooltip">
+    <span data-tooltip-id={id} data-tooltip-class-name="tooltip">
       <Iconly icon={icons.info} />
-      <ReactTooltip id={id} openOnClick>
-        close
+      <ReactTooltip
+        id={id}
+        openOnClick
+        globalCloseEvents={{ escape: true, clickOutsideAnchor: true }}
+      >
+        <div>
+          {content}
+          <span className="close">
+            <Iconly icon={icons.close} />
+          </span>
+        </div>
       </ReactTooltip>
     </span>
   );
