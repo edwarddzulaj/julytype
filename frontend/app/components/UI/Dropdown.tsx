5cec0f83c85ec4abaebaab6e68e9d2cb035007f2
nedkamburov <nedislav.kamburov@gmail.com>
2024-02-09 12:58:23 +0200

    style the dropdown correctly

--- frontend/app/components/UI/Dropdown.tsx
+++ frontend/app/components/UI/Dropdown.tsx
@@ -1,10 +1,24 @@
-import Select from "react-select";
+import Select, { components } from "react-select";
+import Iconly, { icons } from "./Iconly";
+
+const { DropdownIndicator } = components;
+const CustomDropdownIndicator = (props: any) => {
+  const {
+    selectProps: { menuIsOpen },
+  } = props;
+  return (
+    <DropdownIndicator {...props}>
+      {menuIsOpen ? <Iconly icon={icons.chevronDown} /> : <Iconly icon={icons.chevronUp} />}
+    </DropdownIndicator>
+  );
+};
 
 export default function Dropdown({ ...props }) {
   return (
     <Select
       className="dropdown"
       classNamePrefix="react-select"
+      components={{ DropdownIndicator: CustomDropdownIndicator }}
       isSearchable={false}
       escapeClearsValue={true}
       {...props}
